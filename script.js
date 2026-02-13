e1b23aceaf58ba12337a6c1f7451327146c7f215
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-13 12:07:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstChar(text) {
   // your code here
+	if(!text) return "";
+
+	for(let i = 0; i < text.length; i++) {
+		if(text[i] !== " ") {
+			return text[i];
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+// Uncomment the following line to show the prompt popup
+const text = prompt("Enter text:");
+alert(firstChar(text)); 
